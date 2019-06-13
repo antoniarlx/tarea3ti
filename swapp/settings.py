91d408d89e5f814e50756abdeaab64ea39b667d0
Antonia Rodriguez Landeta <antonia@antonias-mbp-2.lan>
2019-06-13 19:26:36 -0400

    De nuevo

--- swapp/settings.py
+++ swapp/settings.py
@@ -120,14 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# STATIC_URL = '/static/'
-#
-# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_URL = '/static/'
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
- STATIC_URL = '/static/'
- STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#  STATIC_URL = '/static/'
+#  STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
